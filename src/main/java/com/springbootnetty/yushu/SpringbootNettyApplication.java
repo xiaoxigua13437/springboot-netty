4a6de8d0555fb13f4485f971ab80d9088a0cbd07
zhaofang <591821761@qq.com>
2020-06-08 16:31:09 +0800

    SpringBoot 整合 Netty

---
+++ src/main/java/com/springbootnetty/yushu/SpringbootNettyApplication.java
@@ -0,0 +1,13 @@
+package com.springbootnetty.yushu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootNettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootNettyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springbootnetty/yushu/SpringbootNettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootnetty.yushu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootNettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
